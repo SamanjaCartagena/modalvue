073c9a5b81c43e3391b4a0df68afa71b8a76f0f6
SamanjaCartagena <cart.samanja09@gmail.com>
2023-01-19 15:46:04 -0500

    modal vue

---
+++ src/components/Modal.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <header class="modal-header">
+        <slot name="header">
+          This is the default title!
+        </slot>
+        <button
+          type="button"
+          class="btn-close"
+          @click="close"
+        >
+          x
+        </button>
+      </header>
+
+      <section class="modal-body">
+        <slot name="body">
+          This is the default body!
+        </slot>
+       </section>
+
+      <footer class="modal-footer">
+        <slot name="footer">
+          This is the default footer!
+        </slot>
+        <button
+          type="button"
+          class="btn-green"
+          @click="close"
+        >
+          Close Modal
+        </button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        close(){
+            this.$emit('close')
+        }
+    }
+
+}
+</script>
+
+<style>
+.modal{
+    background:#FFFFFF;
+    box-shadow:2px 2px 20px 1px;
+    overflow-x:auto;
+    display:flex;
+    flex-direction:column;
+}
+.modal-header,
+.modal-footer
+{
+  padding:15px;display:flex;
+}
+.modal-header{
+    position: relative;
+    border-bottom:1px solid #eeeeee;
+    color:#4AAE9B;
+    justify-content: space-between;
+
+}
+.modal-footer{
+    border-top:1px solid #eeeeee;
+    flex-direction: column;
+    justify-content: flex-end;
+
+}
+.modal-body{
+    position: relative;
+    padding:20px 10px;
+}
+.btn-close{
+    position: relative;
+    top:0;
+    right:0;
+    color:#4AAE9B;
+    border:none;
+    font-size: 20px;
+    padding:10px;
+    cursor:pointer;
+    font-weight:bold;
+    background: transparent;
+}
+.btn-green{
+    color:white;
+    background:#4AAE9B;
+    border:1px solid #4AAE9B;
+    border-radius: 2px;
+}
+</style>
